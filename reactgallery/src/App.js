bf265735d41b7f18a31ed3b38228f5f8ac5f846f
featrd3 <43852603+featrd3@users.noreply.github.com>
2022-12-06 19:02:21 +0100

    fix animation transition

--- reactgallery/src/App.js
+++ reactgallery/src/App.js
@@ -3,11 +3,18 @@ import "./index.css"
 import { ModalGallery } from "./ModalGallery/ModalGallery"
 
 function App() {
-
+  const imagesInGallery = [
+    "https://images.unsplash.com/photo-1668787088962-04516e2829cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=718&q=80",
+    "https://images.unsplash.com/photo-1668790364202-8a0670c90e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1220&q=80",
+    "https://images.unsplash.com/photo-1668718031554-9c05b5d03750?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
+    "https://plus.unsplash.com/premium_photo-1667621221108-d9ff42adee84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    "https://images.unsplash.com/photo-1667835949430-a2516cc93d27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    "https://images.unsplash.com/photo-1668787088962-04516e2829cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=718&q=80"
+]
   return (
     <div>
       <div className="CONTAINER">
-        <ModalGallery/>
+        <ModalGallery numberOfImagesToDisplay = {5} imagesInGallery = {imagesInGallery} />
       </div>
     </div>
 
--- reactgallery/src/ModalGallery/ModalGallery.jsx
+++ reactgallery/src/ModalGallery/ModalGallery.jsx
@@ -6,31 +6,17 @@ import "./fontello/css/fontello.css"
 
 import "./style.css"
 
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
-export const ModalGallery = () => {
-
-    const imagesInGallery = [
-        "https://images.unsplash.com/photo-1668787088962-04516e2829cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=718&q=80",
-        "https://images.unsplash.com/photo-1668790364202-8a0670c90e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1220&q=80",
-        "https://images.unsplash.com/photo-1668718031554-9c05b5d03750?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
-        "https://plus.unsplash.com/premium_photo-1667621221108-d9ff42adee84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
-        "https://images.unsplash.com/photo-1667835949430-a2516cc93d27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-    ]
+export const ModalGallery = ({imagesInGallery, numberOfImagesToDisplay = 3, icon_right = "icon-right-open", icon_left = "icon-left-open"}) => {
 
     const maxIndexOfImagesInGallery = imagesInGallery.length-1;
 
-    const numberOfImagesToDisplay = 3;
-
     var isLessImagesToDisplayThanAvailable = numberOfImagesToDisplay<=maxIndexOfImagesInGallery+1
-
-    var [shiftDirection,setShiftDirection] = useState ('');
 
     var [activeIndex,setActiveIndex] = useState({activeIndexes:0,displayingLastIndex:false,displayingFirstIndex:true});
 
     function handlePreviousClick(){
-        setShiftDirection('right')
-
         var tempIndex = activeIndex.activeIndexes - 1
         if (tempIndex<0) tempIndex = 0
         setActiveIndex({
@@ -41,8 +27,6 @@ export const ModalGallery = () => {
     }
 
     function handleNextClick(){
-        setShiftDirection('left')
-
         var tempIndex = activeIndex.activeIndexes + 1
         if (tempIndex>=maxIndexOfImagesInGallery) tempIndex = maxIndexOfImagesInGallery
         setActiveIndex({
@@ -52,24 +36,24 @@ export const ModalGallery = () => {
         })
     }
 
-    useEffect(()=>{ const myTimeout = setTimeout(()=>setShiftDirection(''), 1000);},[activeIndex])
-
     return(
     <div className="modalGalleryContainer">
         <div className="nav-arrow nav-previous">
             {(isLessImagesToDisplayThanAvailable && !activeIndex.displayingFirstIndex)
-            ?<div className="icon-left-open" onClick={()=>handlePreviousClick()}></div>
+            ?<div className= {icon_left} onClick={()=>handlePreviousClick()}></div>
             :<></>
             }
         </div>
         <div className="gallery">
             <div className="images images-container-transition">
                 {imagesInGallery.map((image,index)=>{
-                    var classTxt = "image-container"
-                    if (activeIndex.activeIndexes<=(index) && activeIndex.activeIndexes+numberOfImagesToDisplay>(index))
-                        classTxt=classTxt+" active"
                     return( 
-                        <div className={classTxt} key={index} data-position={index} data-status={shiftDirection}>
+                        <div className="image-container" style={{
+                            transform:"translateX("+
+                                (-activeIndex.activeIndexes+(maxIndexOfImagesInGallery+1-numberOfImagesToDisplay)/2)*100
+                                +"%)",
+                            width:(100/numberOfImagesToDisplay)+"%"}}
+                            key={index} data-position={index}>
                             <img className="image" src={image}/>
                         </div>)
                 })}
@@ -90,7 +74,7 @@ export const ModalGallery = () => {
         </div>
         <div className="nav-arrow nav-next">
         {(isLessImagesToDisplayThanAvailable && !activeIndex.displayingLastIndex)
-            ?<div className="icon-right-open" key="navigation-right" onClick={()=>handleNextClick()}></div>
+            ?<div className={icon_right} key="navigation-right" onClick={()=>handleNextClick()}></div>
             :<></>
             }  
         </div>
